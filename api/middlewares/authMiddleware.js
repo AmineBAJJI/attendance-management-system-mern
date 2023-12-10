7e2e4c78debe527e8a36a122911510b7faf01429
AmineBAJJI <aminebajji18@gmail.com>
2023-12-11 00:29:38 +0100

    JWT in .env

--- api/middlewares/authMiddleware.js
+++ api/middlewares/authMiddleware.js
@@ -5,7 +5,7 @@ const requireAuth = (req, res, next) => {
 
   // check json web token exists & is verified
   if (token) {
-    jwt.verify(token, "secret asms ensat", (err, decodedToken) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
       if (err) {
         console.log(err.message);
         res.redirect("/login");
