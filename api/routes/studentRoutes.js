145688a564a60eb22d199705c3cb3918f11de141
AmineBAJJI <aminebajji18@gmail.com>
2023-12-14 13:47:04 +0100

    new commit

--- api/routes/studentRoutes.js
+++ api/routes/studentRoutes.js
@@ -5,7 +5,6 @@ const studentController = require("../controllers/studentController");
 router.get("/students", studentController.getAllStudents);
 router.get("/students/:id", studentController.getStudentById);
 router.post("/students", studentController.createStudent);
-
 router.get("/students/class/:class", studentController.getStudentsByClass);
 
 module.exports = router;
